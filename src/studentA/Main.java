59f41aa1ec9c45c095f306a02ab05419021a1278
김수인 <rlatndls30021@gmail.com>
2024-09-25 09:28:27 +0900

    third

--- src/studentA/Main.java
+++ src/studentA/Main.java
@@ -3,6 +3,7 @@ package studentA;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("김수인");
+		World.run();
 	}
 
 }
---
+++ src/studentA/World.java
@@ -0,0 +1,7 @@
+package studentA;
+
+public class World {
+    public static void run() {
+        System.out.println("202114118");
+      }
+}
