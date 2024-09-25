56eabf387a77924081eee2c8bc9b2ad1dcd55857
김수인 <rlatndls30021@gmail.com>
2024-09-25 09:44:28 +0900

    fifth

--- src/studentA/World.java
+++ src/studentA/World.java
@@ -4,4 +4,8 @@ public class World {
     public static void run() {
         System.out.println("202114118");
       }
+      public static void run2() {
+        System.out.println("hello world");
+      }
+      
 }
